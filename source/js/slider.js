0b3a615bb33b2afa0754ea5144f53c4073e613fb
yaroslav em <mr.emelyanenko@gmail.com>
2023-01-18 12:14:27 +0100

    Исправляет отступы

--- source/js/slider.js
+++ source/js/slider.js
@@ -9,20 +9,20 @@ let curtainPlaceStart
 let clientX
 
 function handleGrabCurtain (event) {
-	curtainPlaceStart = +(sliderStyles.getPropertyValue("--curtain-place"))
+curtainPlaceStart = +(sliderStyles.getPropertyValue("--curtain-place"))
   clientX = event.clientX
-	window.addEventListener("pointermove", handleMoveCurtain)
+window.addEventListener("pointermove", handleMoveCurtain)
 }
 
 function handleMoveCurtain (event) {
-	let deltaX = event.clientX - clientX
-	let cursorPlace = curtainPlaceStart + deltaX / slider.clientWidth
-	let curtainPlace = Math.min(Math.max(cursorPlace, 0), 1)
-	slider.style.setProperty("--curtain-place", `${curtainPlace}`)
+    let deltaX = event.clientX - clientX
+    let cursorPlace = curtainPlaceStart + deltaX / slider.clientWidth
+    let curtainPlace = Math.min(Math.max(cursorPlace, 0), 1)
+slider.style.setProperty("--curtain-place", `${curtainPlace}`)
 }
 
 function handleReleaseCurtaine (event) {
-	window.removeEventListener("pointermove", handleMoveCurtain)
+window.removeEventListener("pointermove", handleMoveCurtain)
 }
 
 window.addEventListener("pointerup", handleReleaseCurtaine)
